dec608e4f09e78360cec7048d495a87f290af885
ck <ck@novareto.de>
2014-10-17 09:48:43 +0000

    changes

--- uvc/bootstraptheme/__init__.py
+++ uvc/bootstraptheme/__init__.py
@@ -1,5 +1,15 @@
+# -*- coding: utf-8 -*-
+
 import logging
 logger = logging.getLogger('uvcsite.uvc.bootstraptheme')
 
 def log(message, summary='', severity=logging.DEBUG):
     logger.log(severity, '%s %s', summary, message)
+
+
+from uvc.themes.btwidgets import IBootstrapRequest
+
+
+class IBootstrapThemeRequest(IBootstrapRequest):
+    pass
+
