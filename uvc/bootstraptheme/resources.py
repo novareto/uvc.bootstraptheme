3d4ab17849245e2178c1c0fb229687fdd1c4b63f
Christian Klinger <ck@novareto.de>
2016-02-29 14:50:27 +0100

    add less suppport

--- uvc/bootstraptheme/resources.py
+++ uvc/bootstraptheme/resources.py
@@ -1,9 +1,18 @@
 # -*- coding: utf-8 -*-
 # # Copyright (c) 2007-2011 NovaReto GmbH
 
-from fanstatic import Library, Resource
-from js.bootstrap import bootstrap_theme, bootstrap
+from fanstatic import Library, Resource, Group
+from js.jquery import jquery
 
 library = Library('uvc.bootstraptheme', 'static')
+
+bootstrap_css = Resource(
+    library, 'site.css', compiler='less',
+    source='site.less')
+
+bootstrap_js = Resource(library,'node_modules/bootstrap/dist/js/bootstrap.min.js', depends=[jquery,])
+
+
+bootstrap = Group([bootstrap_css, bootstrap_js])
 
 main_css = Resource(library, 'main.css', depends=[bootstrap])
